018f45323ee524e58c9acaf1f027ebf2f5f797c7
Jay A Carlson <jay.a.carlson@gmail.com>
2016-12-14 11:40:20 -0800

    First Commit -- Hello World to ensure project setup

---
+++ cmake-build-debug/CMakeFiles/DataDictionary_and_DataGrid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataDictionary_and_DataGrid.dir/main.cpp.o"
+  "DataDictionary_and_DataGrid.pdb"
+  "DataDictionary_and_DataGrid"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataDictionary_and_DataGrid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
